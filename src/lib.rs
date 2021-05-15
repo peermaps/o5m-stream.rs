6bee8291ece5d8cefa29e8fe9b9d300dee085626
substack <substack@bits.coop>
2021-05-14 14:07:12 -1000

    use include_str! to inline readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,6 @@
-//! # o5m-stream
-//!
-//! streaming async o5m decoder
-//!
-//! # example
-//!
-//! ``` rust,no_run
-//! use async_std::{prelude::*,fs::File,io};
-//!
-//! type Error = Box<dyn std::error::Error+Send+Sync>;
-//! type R = Box<dyn io::Read+Unpin>;
-//!
-//! #[async_std::main]
-//! async fn main() -> Result<(),Error> {
-//!   let args = std::env::args().collect::<Vec<String>>();
-//!   let infile: R = match args.get(1).unwrap_or(&"-".into()).as_str() {
-//!     "-" => Box::new(io::stdin()),
-//!     x => Box::new(File::open(x).await?),
-//!   };
-//!   let mut stream = o5m_stream::decode(infile);
-//!   while let Some(result) = stream.next().await {
-//!     let r = result?;
-//!     println!["{:?}", r];
-//!   }
-//!   Ok(())
-//! }
-//! ```
+#![feature(extended_key_value_attributes,async_closure,backtrace)]
+#![doc=include_str!("../readme.md")]
 
-#![feature(async_closure,backtrace)]
 use async_std::{prelude::*,stream::Stream,io};
 use std::collections::VecDeque;
 
